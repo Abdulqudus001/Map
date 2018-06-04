e3da6bddef25bbd92faa2b0bb1807b29ef808466
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2018-06-04 15:32:53 +0100

    First commit

--- highchart.js
+++ highchart.js
@@ -1,63 +1,50 @@
-// Prepare demo data
-// Data is joined to map using value of 'hc-key' property by default.
-// See API docs for 'joinBy' for more info on linking data and map.
-var data = [
-    ['ng-ri', 12345],
-    ['ng-kt', 12445],
-    ['ng-so', 23455],
-    ['ng-za', 39876],
-    ['ng-yo', 47564],
-    ['ng-ke', 52345],
-    ['ng-ad', 62343],
-    ['ng-bo', 79876],
-    ['ng-ak', 83456],
-    ['ng-ab', 98776],
-    ['ng-im', 10567],
-    ['ng-by', 11898],
-    ['ng-be', 12898],
-    ['ng-cr', 13344],
-    ['ng-ta', 14455],
-    ['ng-kw', 15234],
-    ['ng-la', 16345],
-    ['ng-ni', 17566],
-    ['ng-fc', 18234],
-    ['ng-og', 19111],
-    ['ng-on', 20123],
-    ['ng-ek', 21123],
-    ['ng-os', 22123],
-    ['ng-oy', 23345],
-    ['ng-an', 24345],
-    ['ng-ba', 25564],
-    ['ng-go', 26123],
-    ['ng-de', 27432],
-    ['ng-ed', 28235],
-    ['ng-en', 29145],
-    ['ng-eb', 30467],
-    ['ng-kd', 31987],
-    ['ng-ko', 32678],
-    ['ng-pl', 33974],
-    ['ng-na', 34347],
-    ['ng-ji', 35790],
-    ['ng-kn', 36126]
-];
+// var data = [
+//     ['ng-ri', 12345],
+//     ['ng-kt', 12445],
+//     ['ng-so', 23455],
+//     ['ng-za', 39876],
+//     ['ng-yo', 47564],
+//     ['ng-ke', 52345],
+//     ['ng-ad', 62343],
+//     ['ng-bo', 79876],
+//     ['ng-ak', 83456],
+//     ['ng-ab', 98776],
+//     ['ng-im', 10567],
+//     ['ng-by', 11898],
+//     ['ng-be', 12898],
+//     ['ng-cr', 13344],
+//     ['ng-ta', 14455],
+//     ['ng-kw', 15234],
+//     ['ng-la', 16345],
+//     ['ng-ni', 17566],
+//     ['ng-fc', 18234],
+//     ['ng-og', 19111],
+//     ['ng-on', 20123],
+//     ['ng-ek', 21123],
+//     ['ng-os', 22123],
+//     ['ng-oy', 23345],
+//     ['ng-an', 24345],
+//     ['ng-ba', 25564],
+//     ['ng-go', 26123],
+//     ['ng-de', 27432],
+//     ['ng-ed', 28235],
+//     ['ng-en', 29145],
+//     ['ng-eb', 30467],
+//     ['ng-kd', 31987],
+//     ['ng-ko', 32678],
+//     ['ng-pl', 33974],
+//     ['ng-na', 34347],
+//     ['ng-ji', 35790],
+//     ['ng-kn', 36126]
+// ];
 
-// new Vue({
-//     el: "#app",
-//     data(){
-//         return{
-//             file: ""
-//         }
-//     }
-// })
-
-// Create the chart
 Highcharts.mapChart('container', {
-    // data: {
-    //     csvURL:'C:/Users/Abdulquddus/devbootcamp/Map/analytics.csv'
-    //     // beforeParse: function (csv) {
-    //     //     return csv.replace(/\n\n/g, '\n');
-    //     // }
-    // },
+    data: {
+         csvURL:'C:/Users/Abdulquddus/devbootcamp/Map/analytics.csv'
+            beforeParse: function (csv) {
+                return csv.replace(/\n\n/g, '\n');
+            }
+    },
     chart: {
         map: 'countries/ng/ng-all'
     },
